e64e70d48c8e5e21849a640c0a2d791ff8640e0a
Stephen Harris <trackmastersteve@users.noreply.github.com>
2019-07-17 20:51:19 -0700

    Update arm0red-net.py

--- arm0red-net.py
+++ arm0red-net.py
@@ -19,4 +19,10 @@ def usage():
   print "arm0red Net Tool"
   print
   print "Usage: arm0red-net.py -t target_host -p port"
-  
+  print "-l --listen                    - listen on [host]:[port] for incoming connections"
+  print "-e --execute=file_to_run       - execute the given file upon receiving a connection"
+  print "-c --command                   - initialize a command shell"
+  print "-u --upload=destination        - upon receiving connaction upload a file and write to destination"
+  print
+  print
+  print 
