ce4e6fa7b2dc8e9e07eb6ba037cfc83c4c2fdc8e
Stephen Harris <trackmastersteve@users.noreply.github.com>
2019-07-19 16:46:50 -0700

    Update arm0red-net.py

--- arm0red-net.py
+++ arm0red-net.py
@@ -25,4 +25,9 @@ def usage():
   print "-u --upload=destination        - upon receiving connaction upload a file and write to destination"
   print
   print
-  print 
+  print "Examples: "
+  print "arm0red-net.py -t 192.168.1.1 -p 5555 -l -c"
+  print "arm0red-net.py -t 192.168.1.1 -p 5555 -l -u=c:\\target.exe"
+  print "arm0red-net.py -t 192.168.1.1 -p 5555 -l -e=\"cat /etc/passwd\""
+  print "echo 'ABCDEFGHI' | ./arm0red-net.py -t 192.168.1.23 -p 135"
+  sys.exit(0)
