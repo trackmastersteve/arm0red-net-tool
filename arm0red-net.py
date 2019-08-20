c58d1bbff46bd250c08ea0875465aeb75a2bef2d
Stephen Harris <trackmastersteve@users.noreply.github.com>
2019-08-20 12:09:12 -0700

    Update arm0red-net.py

--- arm0red-net.py
+++ arm0red-net.py
@@ -32,57 +32,64 @@ def usage():
   print "echo 'ABCDEFGHI' | ./arm0red-net.py -t 192.168.1.23 -p 135"
   sys.exit(0)
 
-  def main():
-    global listen
-    global port
-    global execute
-    global command
-    global upload_destination
-    global target
+def client_sender(buffer):
+  client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  
+  try:
+    # connect to our target host
+    client.connect((target,port))
+  
+def main():
+  global listen
+  global port
+  global execute
+  global command
+  global upload_destination
+  global target
     
-    if not len(sys.argv[1:]):
-      usage()
+  if not len(sys.argv[1:]):
+    usage()
       
-    # read the command line options
-    try:
-      opts, args = getopt.getopt(sys.argv[1:],"hle:t:p:cu:", ["help","listen","execute","target","port","command","upload"])
-    except getopt.GetoptError as err:
-      print str(err)
-      usage()
+  # read the command line options
+  try:
+    opts, args = getopt.getopt(sys.argv[1:],"hle:t:p:cu:", ["help","listen","execute","target","port","command","upload"])
+  except getopt.GetoptError as err:
+    print str(err)
+    usage()
 
-    for o,a in opts:
-      if o in ("-h","--help"):
-        usage()
-      elif o in ("-l","--listen"):
-        listen = True
-      elif o in ("-e","--execute"):
-        execute = a
-      elif o in ("-c","--commandshell"):
-        command True
-      elif o in ("-u","--upload"):
-        upload_destination = a
-      elif o in ("-t","--target"):
-        target = a
-      elif o in ("-p","--port"):
-        port = int(a)
-      else:
-        assert False,"Unhandled Option"
+  for o,a in opts:
+    if o in ("-h","--help"):
+      usage()
+    elif o in ("-l","--listen"):
+      listen = True
+    elif o in ("-e","--execute"):
+      execute = a
+    elif o in ("-c","--commandshell"):
+      command True
+    elif o in ("-u","--upload"):
+      upload_destination = a
+    elif o in ("-t","--target"):
+      target = a
+    elif o in ("-p","--port"):
+      port = int(a)
+    else:
+      assert False,"Unhandled Option"
         
-    # are we going to listen or just send data from stdin?
-    if not listen and len(target) and port > 0:
+  # are we going to listen or just send data from stdin?
+  if not listen and len(target) and port > 0:
       
-      # read in the buffer from the commandline
-      # this will block, so send CTRL-D if not sending imput
-      # to stdin
-      buffer = sys.stdin.read()
+    # read in the buffer from the commandline
+    # this will block, so send CTRL-D if not sending imput
+    # to stdin
+    buffer = sys.stdin.read()
       
-      # send data off
-      client_sender(buffer)
+    # send data off
+    client_sender(buffer)
       
-    # we are going to listen and potentially
-    # upload things, execute commands, and drop a shell back
-    # depending on our command line options above
-    if listen:
-      server_loop()
+  # we are going to listen and potentially
+  # upload things, execute commands, and drop a shell back
+  # depending on our command line options above
+  if listen:
+    server_loop()
       
 main()
